002d9362e0a1891968e830fd680cc50fd5eb0cb5
Jose Adolfo Galdamez <josegalre@pac-rom.com>
2016-04-19 06:35:00 -0500

    Dalvik Hack
    Change-Id: Iba36694113e04840167f753c11019460f35428e8

--- device.mk
+++ device.mk
@@ -45,5 +45,9 @@ PRODUCT_PACKAGES += \
 # Get BCMDHD configs
 $(call inherit-product-if-exists, hardware/broadcom/wlan/bcmdhd/config/config-bcm.mk)
 
+# Dalvik-Hack
+PRODUCT_COPY_FILES += \
+    device/lge/ls990/prebuilt/etc/init.d/01dalvik:system/etc/init.d/01dalvik
+
 # common g3
 $(call inherit-product, device/lge/g3-common/g3.mk)
